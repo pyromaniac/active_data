ebe6227886edc36f175be91362bd7598666f5dc2
pyromaniac <kinwizard@gmail.com>
2015-07-14 09:23:12 +0300

    Return reflection on definition

--- lib/active_data/model/associations.rb
+++ lib/active_data/model/associations.rb
@@ -30,6 +30,7 @@ module ActiveData
             reflection = reflection_class.new self, *args, &block
             reflection.attributes.each { |name, options| attribute(name, options) }
             self.reflections = reflections.merge(reflection.name => reflection)
+            reflection
           end
         end
       end
